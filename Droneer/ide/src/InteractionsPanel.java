587fbc7946df056998f3381094c2557be8ea4226
Zübeyir Bodur <zubeyir99@hotmail.com>
2019-04-30 22:01:04 +0300

    Add files via upload

--- Droneer/ide/src/InteractionsPanel.java
+++ Droneer/ide/src/InteractionsPanel.java
@@ -1,19 +1,23 @@
-import java.awt.Dimension;
+import java.awt.*;
 import javax.swing.*;
 
 public class InteractionsPanel extends JPanel
 {
-   private JLabel information;
+   private JTextArea information;
    
    public InteractionsPanel( String info)
    {
-      information = new JLabel(info);
-      information.setPreferredSize( new Dimension(info.length() * 10, info.length() / 10));
+      setPreferredSize( new Dimension(800, 313) );
+      information = new JTextArea(info, 12, 70);
+      information.setEditable(false);
+      JScrollPane infoPane = new JScrollPane(information);
+      add(infoPane);
+      Font f = new Font(Font.MONOSPACED, Font.PLAIN, 18 );
+      information.setFont(f);
    }
    
    public void update( String info)
    {
       information.setText(info);
-      information.setPreferredSize( new Dimension(info.length() * 10, info.length() / 10));
    }
 }
---
+++ Droneer/ide/src/InteractionsPanelTest.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * BU CLASS TEST AMAÇLIDIR. ÝÞÝMÝZ BÝTÝNCE KURTULACAZ BUNDAN 
+ */
+public class InteractionsPanelTest
+{
+   public static void main( String[] args )
+   {
+      InteractionsPanel test = new InteractionsPanel("Compiler JDK 8.0_191 ready.");
+      JFrame myFrame = new JFrame();
+      myFrame.add(test);
+      myFrame.setLocation(-7, 380);
+      myFrame.pack();
+      myFrame.setVisible(true);
+      
+      for ( int i = 0; i < 200; i++)
+         System.out.println(":D");
+      
+      test.update("36 error found:\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.\nInteger 1000000000 is too large.");
+      
+      
+   }
+}
