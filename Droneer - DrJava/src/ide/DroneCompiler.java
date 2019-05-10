b14e88f8be0608df05aac57df2959bbc48f2ca08
Uğur Erdem <43253140+kugurerdem@users.noreply.github.com>
2019-05-11 01:24:12 +0300

    Add files via upload
    Added some comments to DroneCompiler class that I have written before.

--- Droneer - DrJava/src/ide/DroneCompiler.java
+++ Droneer - DrJava/src/ide/DroneCompiler.java
@@ -4,24 +4,44 @@ import java.util.List;
 import java.io.*;
 import javax.tools.*;
 
+/*
+ * DroneCompiler class that makes compiling the file easier.
+ * @author - Uður Erdem Seyfi
+ * @version - 11.05.2019
+ */
+
 public class DroneCompiler{
-    
+    // properties
     String diagnosticsInfo;
     
+    /*
+     * Default constructor
+     */
     DroneCompiler(){
         diagnosticsInfo = "";
     }
     
-    public boolean compile(String fileName) throws IOException {
+    /*
+     * Method that compiles the java file at given directory
+     * @param directory - path of the java file that is wanted to be compiled ( e.g : "C:\Users\Name\Desktop\Test.java" )
+     * @return - true if the compilation was succesfull, false if there was a problem
+     */
+    public boolean compile(String directory) throws IOException {
+        // Our compiler
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        // Collection of diagnostics
         DiagnosticCollector<JavaFileObject> diagnosticsCollector = new DiagnosticCollector<JavaFileObject>();
+        // FileManager for compiler
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnosticsCollector, null, null);
+        // JavaFiles that are going to be compiled
         Iterable<? extends JavaFileObject> compilationUnits = fileManager
-            .getJavaFileObjectsFromStrings(Arrays.asList(fileName));
+            .getJavaFileObjectsFromStrings(Arrays.asList(directory));
+        // Defining the compilation task
         JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnosticsCollector, null,
                                                              null, compilationUnits);
         boolean success = task.call();
         
+        // Updating the diagnosticsInfo
         this.diagnosticsInfo = "";
         if (!success) {
             List<Diagnostic<? extends JavaFileObject>> diagnostics = diagnosticsCollector.getDiagnostics();
@@ -35,6 +55,10 @@ public class DroneCompiler{
         return success;
     }
     
+    /*
+     * Getter for diagnosticsInfo
+     * @return - diagnosticsInfo
+     */
     public String getDiagnosticsInfo(){
         return this.diagnosticsInfo;
     }
