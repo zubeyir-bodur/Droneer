56c8bd7d98503a5cfebf6a154fb2aaabd1618d2e
Sagiventus <49407824+Sagiventus@users.noreply.github.com>
2019-05-11 18:25:14 +0300

    Missing health bars, drone select, win/lose screen

---
+++ Simplified Drjava Droneer/src/drones/Board.java
@@ -0,0 +1,275 @@
+package drones;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.util.List;
+
+/**
+ * The base JPanel where the drones will fight
+ *
+ * @author Baykam Say, Alp Uneri
+ * @version 11.5.19
+ */
+public class Board extends JPanel implements ActionListener {
+   
+   protected static final int DELAY = 5;
+   
+   private Drone myDrone;
+   private Drone enemyDrone;
+   private Timer timer;
+   private Thread myThread;
+   private Thread enemyThread;
+   //private DroneerMaster master;
+   
+   boolean gameOver;
+   
+   /**
+    * Create a new board.
+    */
+   public Board( Drone myDrone, Drone enemyDrone) {
+
+      //master = new DroneerMaster();
+      
+      setFocusable(true);
+      
+      gameOver = false;
+      
+      this.myDrone = myDrone;
+      this.enemyDrone = enemyDrone;
+      
+      myThread = new Thread(myDrone);
+      enemyThread = new Thread(enemyDrone);
+         
+      initComponents();
+      
+      //setPreferredSize( new Dimension( 700, 500));
+   }
+
+   public Thread getMyThread()
+   {
+      return myThread;
+   }
+   
+   public Thread getEnemyThread()
+   {
+      return enemyThread;
+   }
+   
+   public Timer getTimer()
+   {
+      return timer;
+   }
+   
+   /**
+    * Initialize the components of the board.
+    */
+   private void initComponents() {
+     
+      myThread.start();
+      enemyThread.start();
+      
+      timer = new Timer(DELAY, this);
+      timer.start();
+   }
+
+   /**
+    * Refresh the screen
+    * 
+    * @param ae The action event
+    */
+   @Override
+   public void actionPerformed(ActionEvent ae) {
+      
+      checkGameOver();
+      
+      updateLasers(myDrone.getLasers());
+      updateLasers(enemyDrone.getLasers());
+      
+      updatePlayerDrone();
+      updateEnemyDrone();
+      
+      checkCollisions(enemyDrone);
+      checkCollisions(myDrone);
+      
+      checkCollisions(myDrone.getLasers(), enemyDrone);
+      checkCollisions(enemyDrone.getLasers(), myDrone);
+
+      repaint();
+   }
+   
+   /**
+    * Checks if the game is over, if it is stops the timer.
+    */
+   private void checkGameOver() {
+      if (gameOver) {
+         timer.stop();
+      }
+   }
+   
+   /**
+    * Moves or removes the lasers depending on if they are visible or not.
+    * 
+    * @param lasers The lasers that will be updated
+    */
+   private void updateLasers(List<Laser> lasers) {
+      
+      for (int i = lasers.size() - 1; i >= 0; i--) {
+         
+         Laser l = lasers.get(i);
+         
+         if (l.isVisible()) {
+            l.move();
+         } else {
+            lasers.remove(l);
+         }
+      }
+   }
+   
+   /**
+    * Updates the player drone. Calls the onScannedDrone() method if the player drone sees another 
+    * drone. Ends the game if the player drone is dead.
+    */
+   private void updatePlayerDrone() {
+
+      if (myDrone.isVisible()) {
+         
+         if (myDrone.scan(enemyDrone.getHitbox(), this.getWidth(), this.getHeight())) {
+
+            myDrone.onScannedDrone();
+         }
+         
+      } else {
+         System.out.println("Lost");
+         gameOver = true;
+      }
+   }
+   
+   /**
+    * Updates the enemy drone. Calls the onScannedDrone() method if the enemy drone sees another 
+    * drone. Ends the game if the enemy drone is dead.
+    */
+   private void updateEnemyDrone() {
+
+      if (enemyDrone.isVisible()) {
+         
+         if (enemyDrone.scan(myDrone.getHitbox(), this.getWidth(), this.getHeight())){
+            
+            enemyDrone.onScannedDrone();
+         }
+         
+      } else {
+         System.out.println("Won");
+         gameOver = true;
+      }
+   }
+   
+   /**
+    * Checks the collisions between the boundaries and a drone.
+    *
+    * @param Drone The drone
+    */
+   private void checkCollisions(Drone d) {
+      
+      if (d.getX() < 0) {
+         d.hit();
+         d.setX(1);
+         d.onHitBorder();
+      }
+      
+      if (d.getX() + d.getR() > this.getWidth()) {
+         d.hit();
+         d.setX(this.getWidth() - d.getR() - 1);
+         d.onHitBorder();
+      }
+      
+      if (d.getY() < 0) {
+         d.hit();
+         d.setY(1);
+         d.onHitBorder();
+      }
+      
+      if (d.getY() + d.getR() > this.getHeight()) {
+         d.hit();
+         d.setY(this.getHeight() - d.getR() - 1);
+         d.onHitBorder();
+      }
+   }
+   
+   /**
+    * Checks the collisions between the lasers and a drone.
+    * 
+    * @param lasers The list of lasers
+    * @param Drone The drone
+    */
+   private void checkCollisions(List<Laser> lasers, Drone d) {
+      
+      Ellipse2D hitbox = d.getHitbox();
+      
+      for (Laser l : lasers) {
+         
+         Ellipse2D.Double laserHitbox = l.getHitbox();
+         
+         if (hitbox.intersects(laserHitbox.getBounds2D())) {
+            d.hit();
+            l.setVisible(false);
+         }
+      }
+   }
+   
+   /**
+    * Paint the drones.
+    * 
+    * @param g The graphics object
+    */
+   @Override
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      
+      List<Laser> playerLasers = myDrone.getLasers();
+      List<Laser> enemyLasers = enemyDrone.getLasers();
+      
+      for (Laser l : playerLasers) {
+         drawLaser((Graphics2D) g, l);
+      }
+
+      for (Laser l : enemyLasers) {
+         drawLaser((Graphics2D) g, l);
+      }
+
+      drawDrone((Graphics2D) g, myDrone);
+      drawDrone((Graphics2D) g, enemyDrone);
+      
+      Toolkit.getDefaultToolkit().sync(); // to make the animations smoother
+   }
+
+   /**
+    * Draw the drone at a specified angle.
+    * 
+    * @param g2d The graphics2d object
+    * @param d The drone that will be drawn
+    */
+   private void drawDrone(Graphics2D g2d, Drone d) {
+      
+      // for testing purposes, remove later
+//      g2d.drawOval((int) d.getHitbox().getX(), (int) d.getHitbox().getY(), 
+//              (int) d.getHitbox().getWidth(), (int) d.getHitbox().getHeight());
+      
+      AffineTransform a = AffineTransform.getRotateInstance(d.getAngle(), d.getX() + d.getR() / 2D, 
+              d.getY()+ d.getR() / 2D);
+      g2d.setTransform(a);
+      
+      g2d.drawImage(d.getImage(), (int) d.getX(), (int) d.getY(), this);
+   }
+   
+   /**
+    * Draw the laser.
+    * 
+    * @param g2d The graphics2d object
+    * @param l The laser that will be drawn.
+    */
+   private void drawLaser(Graphics2D g2d, Laser l) {
+      g2d.drawImage(l.getImage(), (int) l.getX(), (int) l.getY(), this);
+   }
+}
---
+++ Simplified Drjava Droneer/src/menus/DesignMenu.java
@@ -0,0 +1,76 @@
+package menus;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.util.ArrayList;
+import ide.*;
+
+/**
+ * Panel for the Design Screen
+ * @author Ege Kaan Gurkan, Alp Uneri
+ * @version 11.5.19
+ */
+
+public class DesignMenu extends JPanel {
+   
+   private DroneerMenuButton backButton;
+   private JLabel explainLabel;
+   private ArrayList<JComponent> components;
+   private JPanel textPanel;
+   private JPanel backPanel;
+   private Editor editor;
+   private JLabel explainLabel2;
+   
+   /**
+    * Creates a JFileChooser and displays it with the given directory
+    */
+   public DesignMenu() {
+     
+      setLayout( new BorderLayout());
+      
+      setVisible(true);
+      backButton = new DroneerMenuButton("< Back");
+      textPanel = new JPanel();
+      backPanel = new JPanel();
+      editor = new Editor();
+      
+      explainLabel2 = new JLabel( "Open the drone you'd like to edit or start from scratch.");
+      explainLabel2.setForeground(Color.BLACK);
+      explainLabel2.setFont(new Font("Monospaced", Font.PLAIN, 30));
+      explainLabel2.setBorder(new EmptyBorder(10,0,10,0));
+      explainLabel2.setAlignmentX(Component.CENTER_ALIGNMENT);
+      
+      textPanel.setLayout(new BorderLayout());
+      textPanel.add(explainLabel2, BorderLayout.NORTH);
+      
+      backPanel.setLayout(new GridLayout(3,3));
+      for(int i = 0; i < 6; i++) {
+         backPanel.add(new JLabel());
+      }
+      backButton.setMargin(new Insets(20,30,20,30));
+      backPanel.add(backButton);
+      
+      components = new ArrayList<JComponent>();
+      components.add( textPanel);
+      components.add(explainLabel2);
+      components.add( editor);
+      
+      for(JComponent c: components) {
+         c.setAlignmentX(Component.CENTER_ALIGNMENT);
+      }
+      
+      add(textPanel, BorderLayout.NORTH);
+      add( editor, BorderLayout.CENTER);
+      add(backPanel, BorderLayout.SOUTH);
+      
+//      add(new JLabel());
+//      add(explainLabel, BorderLayout.NORTH);
+//      add(new JLabel());
+//      add(backButton, BorderLayout.WEST);
+      
+   }
+   
+   public JButton getBackButton() {
+      return backButton;
+   }
+}
